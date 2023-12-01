6f71915495ba6973a23b5a22c4366b4154d0bbc4
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-12-01 22:55:57 +0900

    Refactor test cases and add component name retrieval

--- src/__tests__/get.test.ts
+++ src/__tests__/get.test.ts
@@ -1,17 +1,46 @@
 import { Get } from '../get';
 
+const dirPath = 'src/__testfixtures__/';
+const withPropsComponentPath =
+	'src/__testfixtures__/TestComponentWithProps.tsx';
+const withoutPropsComponentPath =
+	'src/__testfixtures__/TestComponentWithoutProps.tsx';
+
 describe('Get', () => {
-	it('should get all file paths from dir path', () => {
-		// Arrange
-		const dirPath = 'src/__testfixtures__/';
-		// Act
-		const result = Get.allFilePathsFromDirPath(dirPath);
-		// Assert
-		expect(result).toContainEqual(
-			'src/__testfixtures__/TestComponentWithProps.tsx'
-		);
-		expect(result).toContainEqual(
-			'src/__testfixtures__/TestComponentWithoutProps.tsx'
+	describe('allFilePathsFromDirPath', () => {
+		it('should get all file paths from dir path', () => {
+			// Act
+			const result = Get.allFilePathsFromDirPath(dirPath);
+			// Assert
+			expect(result).toContainEqual(
+				'src/__testfixtures__/TestComponentWithProps.tsx'
+			);
+			expect(result).toContainEqual(
+				'src/__testfixtures__/TestComponentWithoutProps.tsx'
+			);
+		});
+	});
+	describe('componentName', () => {
+		test.each<{
+			componentName: string;
+			componentPath: string;
+		}>([
+			{
+				componentName: 'TestComponentWithProps',
+				componentPath: withPropsComponentPath,
+			},
+			{
+				componentName: 'TestComponentWithoutProps',
+				componentPath: withoutPropsComponentPath,
+			},
+		])(
+			'should get the display name of component of $componentName',
+			({ componentName, componentPath }) => {
+				// Act
+				const result = Get.componentName(componentPath);
+				// Assert
+				expect(result).toEqual(componentName);
+			}
 		);
 	});
 });
