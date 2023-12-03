f6b27a8f611c2a0ab22020aeeff6b1f4629a79b6
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-12-03 23:07:33 +0900

    Update Jest and TypeScript configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-	preset: 'ts-jest',
 	testEnvironment: 'node',
 	clearMocks: true,
 	extensionsToTreatAsEsm: ['.ts'],
-
 	moduleNameMapper: {
 		'^(\\.{1,2}/.*)\\.js$': '$1',
 	},
