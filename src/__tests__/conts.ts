3256cf771cc4066ed803f0afa698e44d355e6ac0
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-12-03 23:07:38 +0900

    Delete unnecessary test files

--- src/__tests__/conts.ts
+++
@@ -1,7 +0,0 @@
-const dirPath = 'src/__testfixtures__/';
-const withPropsComponentPath =
-	'src/__testfixtures__/TestComponentWithProps.tsx';
-const withoutPropsComponentPath =
-	'src/__testfixtures__/TestComponentWithoutProps.tsx';
-
-export { dirPath, withPropsComponentPath, withoutPropsComponentPath };
