e458109419477f7fdb89be40763a9572db1f55b2
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-12-03 14:32:44 +0900

    Add propRequired method to Is class

--- src/is.ts
+++ src/is.ts
@@ -1,7 +1,12 @@
 import fs from 'fs';
+import { PropDescriptor } from 'react-docgen/dist/Documentation.js';
 
 export class Is {
 	static pathADirPath(path: string) {
 		return fs.statSync(path).isDirectory();
+	}
+
+	static propRequired(prop: PropDescriptor){
+		return prop.required;
 	}
 }
