d8be3ce8187373562dea82699f8a186b0011f8a9
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-12-19 22:47:56 +0900

    add execute.test.ts

--- src/execute.test.ts
+++ src/execute.test.ts
@@ -2,8 +2,11 @@ import { describe, expect, it, vi } from 'vitest';
 import { Get } from './get.js';
 import { Generate } from './generate.js';
 import { Execute } from './execute.js';
+import { Log } from './log.js';
 
+// Get mocks
 const mockGetComponentSignatures = vi.spyOn(Get, 'componentSignatures');
+// Generate mocks
 const mockGenerateStoryFileDescriptorFromPath = vi.spyOn(
 	Generate,
 	'storyFileDescriptorFromPath'
@@ -11,20 +14,82 @@ const mockGenerateStoryFileDescriptorFromPath = vi.spyOn(
 const mockGenerateStorybookTemplate = vi.spyOn(Generate, 'storybookTemplate');
 const mockGenerateStoryPropArgs = vi.spyOn(Generate, 'storyPropArgs');
 const mockGenerateStory = vi.spyOn(Generate, 'story');
+// Log mocks
+const mockLogExecutionResult = vi.spyOn(Log, 'executionResult');
 
-describe('execute', () => {
-	it('should call core functionality', () => {
-		// Arrange
-		const dummyFilePath = 'src/__testfixtures__/TestComponentWithoutProps.tsx';
-		// Act
-		Execute.on(dummyFilePath, {
-			relativeTitle: false,
+describe('Execute', () => {
+	describe('on', () => {
+		it('should call core functionality', () => {
+			// Arrange
+			const dummyFilePath =
+				'src/__testfixtures__/TestComponentWithoutProps.tsx';
+			// Act
+			Execute.on(dummyFilePath, {
+				relativeTitle: false,
+			});
+			// Assert
+			expect(mockGetComponentSignatures).toHaveBeenCalled();
+			expect(mockGenerateStoryFileDescriptorFromPath).toHaveBeenCalled();
+			expect(mockGenerateStorybookTemplate).toHaveBeenCalled();
+			expect(mockGenerateStoryPropArgs).toHaveBeenCalled();
+			expect(mockGenerateStory).toHaveBeenCalled();
 		});
-		// Assert
-		expect(mockGetComponentSignatures).toHaveBeenCalled();
-		expect(mockGenerateStoryFileDescriptorFromPath).toHaveBeenCalled();
-		expect(mockGenerateStorybookTemplate).toHaveBeenCalled();
-		expect(mockGenerateStoryPropArgs).toHaveBeenCalled();
-		expect(mockGenerateStory).toHaveBeenCalled();
+	});
+	describe('program', () => {
+		it('should run once when path is a filepath', () => {
+			// Arrange
+			const mockExecuteOn = vi.spyOn(Execute, 'on');
+			const targetFileName = 'TestComponentWithoutProps';
+			const dummyFilePath = 'src/__testfixtures__';
+			// Act
+			Execute.program(`${dummyFilePath}/${targetFileName}.tsx`, {
+				relativeTitle: false,
+			});
+			// Assert
+			expect(mockExecuteOn).toHaveBeenCalledTimes(1);
+			expect(mockLogExecutionResult).toHaveBeenCalledWith(
+				[`${dummyFilePath}/${targetFileName}.stories.tsx`],
+				[]
+			);
+		});
+
+		// TODO: should skip story files by default
+		it.fails('should run multiple times when path is a dirpath', () => {
+			// Arrange
+			const mockExecuteOn = vi.spyOn(Execute, 'on');
+			const dummyDirPath = 'src/__testfixtures__';
+			// Act
+			Execute.program(dummyDirPath, {
+				relativeTitle: false,
+			});
+			// Assert
+			expect(mockExecuteOn).toHaveBeenCalledTimes(2);
+			expect(mockLogExecutionResult).toHaveBeenCalledWith(
+				[
+					'src/__testfixtures__/TestComponentWithProps.stories.tsx',
+					'src/__testfixtures__/TestComponentWithoutProps.stories.tsx',
+				],
+				[]
+			);
+		});
+
+		it('should throw error when target file is not a react component', () => {
+			// Arrange
+			const nonComponentFilePath = 'src/execute.ts';
+			// Act
+			Execute.program(nonComponentFilePath, {
+				relativeTitle: false,
+			});
+			// Assert
+			expect(mockLogExecutionResult).toHaveBeenCalledWith(
+				[],
+				[
+					{
+						filePath: nonComponentFilePath,
+						error: new Error('No suitable component definition found.'),
+					},
+				]
+			);
+		});
 	});
 });
